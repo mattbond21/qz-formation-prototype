875ce2009c7ecf52ccc276095d2f2da48f24fa1c
Matt Bond <matt@mattbond.com.au>
2020-02-21 01:11:02 -0800

    new assets and bootstrap components

--- js/main.js
+++ js/main.js
@@ -1,88 +1,32 @@
-/* image zoom - add data-action="zoom" to images */
-
-+function ($) {
-  'use strict';
-
-  // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
-  // ============================================================
-
-  function transitionEnd() {
-    var el = document.createElement('bootstrap')
-
-    var transEndEventNames = {
-      WebkitTransition : 'webkitTransitionEnd',
-      MozTransition    : 'transitionend',
-      OTransition      : 'oTransitionEnd otransitionend',
-      transition       : 'transitionend'
-    }
-
-    for (var name in transEndEventNames) {
-      if (el.style[name] !== undefined) {
-        return { end: transEndEventNames[name] }
-      }
-    }
-
-    return false // explicit for ie8 (  ._.)
+// CHECKBOX
+$("button.checkbox").click(function(){
+  if ($(this).hasClass("selected") == true){
+    $(this).removeClass("selected");
+  } else {
+    $(this).addClass("selected");
   }
+});
 
-  // http://blog.alexmaccaw.com/css-transitions
-  $.fn.emulateTransitionEnd = function (duration) {
-    var called = false
-    var $el = this
-    $(this).one('bsTransitionEnd', function () { called = true })
-    var callback = function () { if (!called) $($el).trigger($.support.transition.end) }
-    setTimeout(callback, duration)
-    return this
+  //RADIO
+$(".radio").click(function(){
+  if ($(this).hasClass("selected") == true){
+    $(this).removeClass("selected");
+  }else{
+    $(this).addClass("selected");
   }
+});
 
-  $(function () {
-    $.support.transition = transitionEnd()
+$(document).ready(function() {
+  $('.drawer').drawer();
+});
 
-    if (!$.support.transition) return
-
-    $.event.special.bsTransitionEnd = {
-      bindType: $.support.transition.end,
-      delegateType: $.support.transition.end,
-      handle: function (e) {
-        if ($(e.target).is(this)) return e.handleObj.handler.apply(this, arguments)
-      }
-    }
-  })
-
-  // Video playback
-
-  $('.demo-video').addClass('demo-video-paused');
-  $('.demo-video').bind('click', function () {
-      var video = $(this).find('video').get(0);
-      if (video.paused) {
-          video.play();
-      } else {
-          video.pause();
-      }
-  });
-
-  $('.demo-video').bind('mouseenter', function () {
-      var video = $(this).find('video').get(0);
-      if (!video.paused) {
-          $(this).addClass('demo-video-hover');
-      }
-  });
-
-  $('.demo-video').bind('mouseleave', function () {
-      var video = $(this).find('video').get(0);
-      if (!video.paused) {
-          $(this).removeClass('demo-video-hover');
-      }
-  });
-
-  $('.demo-video video').on('pause', function() {
-      $(this).parent().addClass('demo-video-paused');
-      $(this).parent().removeClass('demo-video-hover');
-  });
-
-  $('.demo-video video').on('play', function() {
-      $(this).parent().removeClass('demo-video-paused');
-  });
-
-
-}(jQuery);
+$(function() {
+    $('ul.o-list-ui li a').on('click', function(e){
+        var $link = $(this);
+        var $parentNode = $link.parent();
+        var linkLeavesPage = $link.attr('href');
+        if (!linkLeavesPage) {
+            $parentNode.toggleClass('nav-tree-node-is-open');
+        }
+    });
+});
